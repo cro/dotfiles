8ae0865532e901b45d41a645954784712cdd64ff
C. R. Oldham <cro@ncbt.org>
2014-04-29 10:18:35 -0600

    Add vi mode

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -75,7 +75,8 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_prompt_host (set_color $fish_color_host)
     end
 
-    echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" "$__fist_color_user" (prompt_pwd) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+    # echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" "$__fist_color_user" (prompt_pwd) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+    echo (fish_vi_prompt)
 end
 
 
