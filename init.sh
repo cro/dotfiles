30c1a1958ae4d5e0da6fe5f736df33ebf612dde7
C. R. Oldham <cro@ncbt.org>
2014-07-03 11:05:53 -0600

    update init.sh

--- init.sh
+++ init.sh
@@ -9,24 +9,39 @@ sudo pip install virtualenvwrapper rope ropevim pudb
 # sudo pacman -Syu
 # sudo pacman -S vim git zsh python-setuptools vim-gtk gnome-terminal
 
+# Get my dotfiles
 git clone https://github.com/cro/dotfiles /root/dotfiles
 
-chsh -s /bin/zsh
-sudo echo "export ZDOTDIR=~/.zshell" >> /etc/zsh/zshenv
-mkdir -p ~/vimified/tmp/undo
-mkdir -p ~/vimified/tmp/backup
-mkdir -p ~/vimified/tmp/swap
-mkdir -p ~/vimified/bundle
+# Change my shell to zsh
+if [ -e /bin/zsh ]; then
+    chsh -s /bin/zsh
+else
+    chsh -s /usr/bin/zsh
+fi
 
-ln -s ~/vimified ~/.vim
+# Make sure zsh knows where to look for startup files
+if [ -e /etc/zsh/zshenv ]; then
+    sudo echo "export ZDOTDIR=~/.zshell" >> /etc/zsh/zshenv
+fi
+
+# Get rid of default files
 rm ~/.profile
 rm ~/.bashrc
+
 ln -s ~/dotfiles/.zshell ~/.zshell
 ln -s ~/dotfiles/.profile ~/.profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.config ~/.config
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 
+mkdir -p ~/vimified/tmp/undo
+mkdir -p ~/vimified/tmp/backup
+mkdir -p ~/vimified/tmp/swap
+mkdir -p ~/vimified/bundle
+
+ln -s ~/vimified ~/.vim
 ln -s ~/dotfiles/vimified/vimrc ~/.vimrc
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+
 vim +BundleInstall +qall
