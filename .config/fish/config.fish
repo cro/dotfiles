c81d38ba375994ce7f1035a425fab33a170df12c
C. R. Oldham <cro@ncbt.org>
2015-01-05 13:41:34 -0700

    Lots of fixes & move tacklebox into dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,6 @@ set -g VIRTUALFISH_HOME ~/src/ve
 set -g VIRTUALFISH_COMPAT_ALIASES # uncomment for virtualenvwrapper-style commands
 set -gx PATH ~/node_modules/.bin $PATH
 set -gx EDITOR /opt/local/bin/vim
-# . ~/.config/fish/virtualfish/virtual.fish
-# optional plugins
-# . ~/.config/fish/virtualfish/auto_activation.fish
-# . ~/.config/fish/virtualfish/global_requirements.fish
-# . ~/.config/fish/functions/vfhooks.fish
-# . ~/.config/fish/functions/rvm.fish
 set fish_key_bindings fish_vi_key_bindings
 
 # Make fish be quiet on start
@@ -27,21 +21,21 @@ if test -f ~/.ec2/api
 end
 
 # Paths to your tackle
-set fish_path ~/.tackle ~/.tacklebox
+set tacklebox_path ~/.tackle
 
 # Theme
-set fish_theme entropy
+set tacklebox_theme entropy
 
 # Which modules would you like to load? (modules can be found in ~/.tackle/modules/*)
 # Custom modules may be added to ~/.tacklebox/modules/
-# Example format: set fish_modules virtualfish virtualhooks
+# Example format: set tacklebox_modules virtualfish virtualhooks
 
-set fish_modules virtualfish virtualhooks
+set tacklebox_modules virtualfish virtualhooks
 
 # Which plugins would you like to enable? (plugins can be found in ~/.tackle/plugins/*)
 # Custom plugins may be added to ~/.tacklebox/plugins/
 # Example format: set fish_plugins python extract
-set fish_plugins python extract
+set tacklebox_plugins python extract
 
 # Load Tacklebox configuration
 . ~/.tacklebox/tacklebox.fish
