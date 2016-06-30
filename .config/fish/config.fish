85afe61705fae4a770d177bda82563800afd1d68
C. R. Oldham <cro@ncbt.org>
2016-06-30 17:21:37 -0600

    Adding fbsd fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 set -g WORKON_HOME ~/src/ve
 set -g VIRTUALFISH_HOME ~/src/ve
 set -g VIRTUALFISH_COMPAT_ALIASES # uncomment for virtualenvwrapper-style commands
-set -gx PATH ~/node_modules/.bin $PATH
+set -gx PATH ~/bin ~/dotfiles/bin ~/node_modules/.bin $PATH
 set -gx EDITOR /usr/local/bin/emacsclient
 
 # eval (/usr/bin/python -m virtualfish compat_aliases)
