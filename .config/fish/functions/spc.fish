7f1522096384c64d7086fa3d48a0b0d337c8ba2f
C. R. Oldham <cro@ncbt.org>
2016-10-16 08:51:40 -0600

    New commands

--- .config/fish/functions/spc.fish
+++
@@ -1,7 +0,0 @@
-function spc --description='Spacemacs Client'
-  /usr/local/bin/emacsclient -n $argv
-end
-
-function spct --description='Spacemacs Client, wait for buffer'
-  /usr/local/bin/emacsclient $argv
-end
